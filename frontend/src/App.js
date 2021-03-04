7cb54856a504c2cf3bfa115ca9c4ca2865635e98
Gabi <gabee_88@yahoo.com>
2021-03-04 16:31:25 +0100

    chore: added react-router-dom package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <h1>Register</h1>
+
+        <Switch>
+          <Route path="/">
+
+          </Route>
+          <Route path="/register">
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
