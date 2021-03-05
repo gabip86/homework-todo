d10dadfa977fbed5e37994c9b458edf20529c2f4
Gabi <gabee_88@yahoo.com>
2021-03-05 13:47:34 +0100

    style: added style to the form

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,23 +1,27 @@
 import React from 'react'
-import { Form, Button } from 'react-bootstrap'
+import { Container, Form, Button, Row, Col } from 'react-bootstrap'
 
 function Register() {
   return (
-    <div>
+    <Container>
       <Form>
-        <Form.Group>
-          <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Username"></Form.Control>
+        <Form.Group as={Row}>
+          <Form.Label column sm={2}>Username</Form.Label>
+          <Col sm={10}>
+            <Form.Control type="text" placeholder="Username"></Form.Control>
+          </Col>
         </Form.Group>
-        <Form.Group>
-          <Form.Label>Password</Form.Label>
-          <Form.Control tpye="password" placeholder="Password"></Form.Control>
+        <Form.Group as={Row}>
+          <Form.Label column sm={2}>Password</Form.Label>
+          <Col sm={10}>
+            <Form.Control tpye="password" placeholder="Password"></Form.Control>
+          </Col>
         </Form.Group>
         <Button variant="primary" type="submit">
           Register
         </Button>
       </Form>
-    </div>
+    </Container>
   )
 }
 
