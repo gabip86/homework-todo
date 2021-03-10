f7389db102ccf659b59ae3ef2ec11a14dccfa13b
Gabi <gabee_88@yahoo.com>
2021-03-10 22:33:52 +0100

    feat: created MainTodo component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import {
   BrowserRouter as Router,
@@ -8,70 +7,29 @@ import {
 import Header from './components/Header'
 import Register from './components/Register'
 import Login from './components/Login'
-import FormTodo from './components/FormTodo'
-import Todo from './components/Todo'
-import { Card } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import MainTodo from './components/MainTodo'
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      text: 'This is my first todo',
-      isDone: false
-    }
-  ])
-
-  const addTodo = text => {
-    const newTodos = [...todos, { text }]
-    setTodos(newTodos)
-  }
-
-  const markTodo = index => {
-    const newTodos = [...todos]
-    newTodos[index].isDone = true
-    setTodos(newTodos)
-  }
-
-  const removeTodo = index => {
-    const newTodos = [...todos]
-    newTodos.splice(index, 1)
-    setTodos(newTodos)
-  }
-
   return (
     <div className="app">
-      <Router>
-        <Header />
-        <h1 className="text-center mb-4">Todo List</h1>
-        <FormTodo addTodo={addTodo} />
-        <div>
-          {todos.map((todo, index) => (
-            <Card>
-              <Card.Body>
-                <Todo
-                  key={index}
-                  index={index}
-                  todo={todo}
-                  markTodo={markTodo}
-                  removeTodo={removeTodo}
-                />
-              </Card.Body>
-            </Card>
-          ))}
-        </div>
-
-        <Switch>
-          <Route exact path="/">
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+      <Header />
+        <Router>
+          <Switch>
+            <Route exact path="/">
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/todos">
+              <MainTodo />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,6 +11,7 @@ function Header() {
           <Nav className="mr-auto">
             <Nav.Link href="/register">Register</Nav.Link>
             <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/todos">Todos</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
